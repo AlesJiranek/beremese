0a290c427c3e9053361a45534485c8873e2df91b
Travis CI <a.jiranek@gmail.com>
2017-12-28 12:28:13 +0000

    Deploy to GitHub Pages: fb61874fade3035dcd16c3334e0598c674c9c3e5

--- _next/b5f2ce99-8df0-4d23-8395-9bf0306e4f12/page/index.js
+++ _next/b5f2ce99-8df0-4d23-8395-9bf0306e4f12/page/index.js
@@ -1,6 +1,6 @@
 
           window.__NEXT_REGISTER_PAGE('/', function() {
-            var comp = module.exports=webpackJsonp([2],[],[211]);
+            var comp = module.exports=webpackJsonp([2],[],[225]);
             return { page: comp.default }
           })
         
