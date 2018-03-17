5cf7716daf68b6f898dabdfc604b9f2714d75afc
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-03-17 10:35:18 +0000

    Deploy AlesJiranek/beremese to github.com/AlesJiranek/beremese.git:gh-pages

--- _next/a02df2d4-c787-4ae4-9079-3c15309aeaa8/page/co-na-sebe/index.js
+++
@@ -1,6 +0,0 @@
-
-          window.__NEXT_REGISTER_PAGE('/co-na-sebe', function() {
-            var comp = module.exports=webpackJsonp([10],{430:function(e,t,l){e.exports=l(431)},431:function(e,t,l){"use strict";function n(e){return e&&e.__esModule?e:{default:e}}Object.defineProperty(t,"__esModule",{value:!0});var a=l(0),u=n(a),o=l(48),d=n(o),r=l(49),s=n(r),m=l(68),c=n(m),i=l(3),p=(n(i),function(){return u.default.createElement(d.default,null,u.default.createElement(s.default,{backgroundImage:"/static/images/img_4823-small.jpg",small:!0,overlay:.1},u.default.createElement("h1",null,"Co na sebe?")),u.default.createElement(c.default,null,u.default.createElement(m.TextContent,null,u.default.createElement("blockquote",null,u.default.createElement("p",null,"Největším darem pro nás bude to, že s námi strávíte náš den! A toho si opravdu vážíme! Žádné vázy, jídelní soupravy, slánky ani mixéry nejsou potřeba, neboť si myslíme, že všeho máme dost. Pokud i přesto na nějakém daru trváte, oceníme spíše příspěvek na naše další společné cesty za dobrodružstvím. Uvidíme co zvítězí, prosluněná cesta na Honolulu nebo expedice v Himalájích :D")))))});t.default=p}},[430]);
-            return { page: comp.default }
-          })
-        
