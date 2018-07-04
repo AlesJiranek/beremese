a29a52b7002317876b9dd4f7e43d89b6a57f0510
Aleš Jiránek <a.jiranek@gmail.com>
2018-07-04 12:15:02 +0200

    Fix IE

--- next.config.js
+++ next.config.js
@@ -9,5 +9,19 @@ module.exports = {
       '/dary': { page: '/dary' },
       '/vzpominky': { page: '/vzpominky' }
     };
+  },
+  webpack: function(cfg) {
+    const originalEntry = cfg.entry;
+    cfg.entry = async () => {
+      const entries = await originalEntry();
+
+      if (entries['main.js']) {
+        entries['main.js'].unshift('./lib/Polyfills.js');
+      }
+
+      return entries;
+    };
+
+    return cfg;
   }
 };
